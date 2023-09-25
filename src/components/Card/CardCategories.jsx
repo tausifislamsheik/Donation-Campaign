9b6fdaf15ca9610accae14ebad1b0bb485d4f891
Tausif Islam Sheik <you@example.com>
2023-09-26 00:48:52 +0600

    Cards details added

--- src/components/Card/CardCategories.jsx
+++ src/components/Card/CardCategories.jsx
@@ -1,8 +1,49 @@
+import { setItem } from "localforage";
+import Swal from "sweetalert2";
 
 
 const CardCategories = ({card}) => {
 
     const {id, picture, title, description, price} = card || {}
+
+    const handleAddToDonation = () => {
+
+       const addedDonationArray = [];
+
+        const donationItems = JSON.parse(localStorage.getItem('donation'))
+
+      if(!donationItems){
+        addedDonationArray.push(card)
+        localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+        Swal.fire(
+            'Donation Successfull!',
+            'You clicked the button!',
+            'success'
+          )
+      }else{
+
+        const isExist = donationItems.find(card => card.id === id)
+
+        if(!isExist){
+            addedDonationArray.push(...donationItems, card)
+            localStorage.setItem('donation', JSON.stringify(addedDonationArray))
+            Swal.fire(
+                'Donation Successfull!',
+                'You clicked the button!',
+                'success'
+              )
+        }else{
+            Swal.fire(
+                'No Dublicate!',
+                'You clicked the button!',
+                'error'
+            )
+        }
+        
+      }
+
+        // localStorage = setItem('test', )
+    }
 
 
     return (
@@ -13,7 +54,7 @@ const CardCategories = ({card}) => {
      </figure>
   <div className="card-body">
     <div className="card-actions">
-      <button className="btn btn-primary">Donate {price}</button>
+      <button onClick={handleAddToDonation} className="btn btn-primary">Donate {price}</button>
     </div>
     <h2 className="card-title font-bold">{title}</h2>
     <p>{description}</p>
